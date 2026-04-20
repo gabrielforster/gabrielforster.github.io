0b3127bf0f287aa41dadd5858d8b6f55b5b4391f
Gabriel Rocha <rochafrgabriel@gmail.com>
2026-04-20 19:06:14 -0300

    improve layout of home page
    Change layout from home page to display work section and projects before
    blog posts.

--- src/consts.ts
+++ src/consts.ts
@@ -5,7 +5,7 @@ export const SITE: Site = {
   FULLNAME: "Gabriel Rocha",
   EMAIL: "contatogabrielrochaf@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 3,
-  NUM_WORKS_ON_HOMEPAGE: 2,
+  NUM_WORKS_ON_HOMEPAGE: 3,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
